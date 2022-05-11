ae68e2c38feaaa5c7a6b1f94c4bf1ec91d685274
Jang <jjang12@occ.cccd.edu>
2022-05-11 10:36:28 -0700

    Added coolFeature method

--- Prog2.java
+++ Prog2.java
@@ -3,5 +3,11 @@ public class Prog2
     public static void main(String[] args)
     {
         System.out.println("Hi");
+        coolFeature();
+    }
+
+    public static void coolFeature()
+    {
+        System.out.println("Cool, isn't it?");
     }
 }
